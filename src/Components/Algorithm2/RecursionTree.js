21979b9e33820df1d82d2389c0dfbe76aa021d5b
Gerencsér Csaba <csabiger@gmail.com>
2023-09-13 18:14:32 +0200

    AutumnSave0913LT

--- src/Components/Algorithm2/RecursionTree.js
+++ src/Components/Algorithm2/RecursionTree.js
@@ -7,21 +7,21 @@ export default function RecursionTree() {
     var N = document.getElementById("B_element").value;
     var n = document.getElementById("N_element").value;
 
-    var n_elem = `(n/ ${N}) <sup>i</sup>`; //hiba javitas
+    var n_elem = `n/${N}<sup>i</sup>`; //hiba javitas
     var magassag = `log<sub>${N}</sub> n`;
-    var levelszameredmeny = `nlog<sub>${N}</sub> ${T}`;
+    var levelszameredmeny = `nlog<sub>${N}</sub><sup>${T}</sup>`;
 
     if (n === "n2") {
-      n_elem = `(${n_elem})<sup>2</sup>`;
+      n_elem = `${n_elem}<sup>2</sup>`;
     }
     if (n === "n3") {
-      n_elem = `(${n_elem})<sup>3</sup>`;
+      n_elem = `${n_elem}<sup>3</sup>`;
     }
     if (n === "n4") {
-      n_elem = `(${n_elem})<sup>4</sup>`;
+      n_elem = `${n_elem}<sup>4</sup>`;
     }
     if (n === "n5") {
-      n_elem = `(${n_elem})<sup>5</sup>`;
+      n_elem = `${n_elem}<sup>5</sup>`;
     }
 
     document.getElementById(
@@ -32,7 +32,7 @@ export default function RecursionTree() {
     ).innerHTML = `Az i-edik szinten a csúcsok száma: ${T}<sup>i</sup>`;
     document.getElementById(
       "tree-high"
-    ).innerHTML = `Az i-edik szint összköltsége: ( n/${N} )<sup>i</sup>`;
+    ).innerHTML = `Az i-edik szint összköltsége: ${T}<sup>i</sup> * n/${N}<sup>i</sup>`;
     document.getElementById("leaf-number").innerHTML = `Levelek száma:
       ${T}
       <sup>
@@ -43,7 +43,7 @@ export default function RecursionTree() {
     ).innerHTML = `A fa magassága: ${magassag}`;
     document.getElementById(
       "final"
-    ).innerHTML = `Összeképlet: T(n) = <sub>i=0</sub> Σ <sup>log<sub>${N}</sub></sup><sup> i-1</sup> ${T}<sup>i</sup> * ${n_elem} * ${levelszameredmeny}`;
+    ).innerHTML = `Összegképlet: T(n) = <sub>n=0</sub> Σ <sup>log<sub>${N}</sub></sup><sup> n-1</sup> ${T}<sup>i</sup> * ${n_elem} + ${levelszameredmeny}`;
   }
 
   return (
@@ -53,51 +53,48 @@ export default function RecursionTree() {
       >
         Rekurziós fa módszer
       </h1>
-      <div class="form-group">
-        <div class="container">
-          <div class="row justify-content-center text-center">
-            <div class="col-2">
+      <div className="form-group">
+        <div className="container">
+          <div className="row justify-content-center text-center">
+            <div className="col-2">
               <b style={{ color: "white" }}>A:</b>
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 id="A_element"
                 placeholder="A elem"
               />{" "}
-              <small class="form-text text-white">(T elem szorzata)</small>
             </div>
-            <div class="col-2">
+            <div className="col-2">
               {" "}
               <b style={{ color: "white" }}>B:</b>
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 id="B_element"
                 placeholder="B elem"
               />
-              <small class="form-text text-white">(n elem szorzata)</small>
             </div>
-            <div class="col-2">
+            <div className="col-2">
               {" "}
               <b style={{ color: "white" }}>f(n): </b>
               <input
                 type="text"
-                class="form-control"
+                className="form-control"
                 id="N_element"
                 placeholder="n elem"
               />
-              <small class="form-text text-white">(n kitevős tag)</small>
             </div>
           </div>
           <br />
-          <div class="row justify-content-center text-center">
+          <div className="row justify-content-center text-center">
             <Button variant="btn btn-outline-warning" onClick={Result}>
               Kiszámol
             </Button>
           </div>
         </div>
       </div>
-      <div style={{ color: "white", paddingLeft:"40%" }}>
+      <div style={{ color: "white", paddingLeft:"45%" }}>
         <br />
         <b id="one-edge"></b>
         <br />
